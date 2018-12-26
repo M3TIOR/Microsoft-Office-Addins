2935018c5db35a272b0669177e2d0801b76433c4
Ruby Allison Rose <jtimmerman32@gmail.com>
2018-12-25 21:40:56 -0800

    Migrated previously completed work to new repo with attribution.

--- Outlook/maildown/function-file/function-file.js
+++ Outlook/maildown/function-file/function-file.js
@@ -9,3 +9,32 @@ Office.initialize = reason => {
 };
 
 // Add any ui-less function here
+function convertMarkdown(event){
+    // This code is ran on a button press only. (I hope)
+    var item = Office.context.mailbox.item;
+    var content = item.body;
+
+	// Is enum, no instanceof (*u*)
+	if (item.itemType == Office.MailboxEnums.ItemType.Message){
+
+		// Load Showdown and make it spicy!
+		var converter = new showdown.Converter();
+		//converter.setFlavor('github');
+
+		content.getAsync(Office.CoercionType.Text, {}, (result) => {
+			if (result.status == "succeeded"){
+				let html = converter.makeHtml(result.value);
+
+				content.setAsync(html, {coercionType:"html", }, (result) => {
+					// We have to have some kind of error handeling.
+					if(result.status != "succeeded")
+						console.log(result);
+				});
+			}
+			else {
+				console.log(result);
+			}
+
+		});
+	}
+}
